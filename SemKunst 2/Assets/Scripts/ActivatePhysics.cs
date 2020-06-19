f62ac6f26e5c596ad9cddef3c75171de08c629c2
Unknown <jeremy.koskamp@live.nl>
2020-06-19 21:39:41 +0200

    [/]Improved Wall

---
+++ SemKunst 2/Assets/Scripts/ActivatePhysics.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatePhysics : MonoBehaviour
+{
+    Rigidbody rb;
+    Vector3 originalPos;
+    Quaternion originalRotation;
+    private Vector3 velocity = Vector3.zero;
+
+    private float steps = .3f;
+    bool canReturn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        originalPos = this.GetComponent<Transform>().position;
+        originalRotation = this.GetComponent<Transform>().rotation;
+    }
+
+    void Update()
+    {
+        if (canReturn)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, originalPos, ref velocity, steps);
+            transform.rotation = Quaternion.Slerp(transform.rotation, originalRotation, Time.time * steps / 100);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canReturn = false;
+            rb.isKinematic = false;
+            StartCoroutine(TurnKinematic());
+        }
+    }
+
+    IEnumerator TurnKinematic()
+    {
+        yield return new WaitForSeconds(5f);
+        canReturn = true;
+        rb.isKinematic = true;
+    }
+}
--- SemKunst 2/Assets/Scripts/MoreArmsAccordingToDistance.cs
+++ SemKunst 2/Assets/Scripts/MoreArmsAccordingToDistance.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class MoreArmsAccordingToDistance : MonoBehaviour
 {
+    [SerializeField] Vector3 minPos;
+    [SerializeField] Vector3 maxPos;
+
+    public List<Transform> targets;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < targets.Count; i++)
+        {
+            var distance = Vector3.Distance(this.transform.position, targets[i].transform.position);
+            Debug.Log(distance);
+            //float newYpos = Remap(distance,0);
+            this.transform.position = new Vector3(-distance / 4, transform.position.y, transform.position.z);
+
+            Vector3 lTargetDir = m_Target.position - transform.position;
+            lTargetDir.y = 0.0f;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lTargetDir), Time.time * m_Speed);
+        }
     }
+
+    public float Remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+
+    [SerializeField]
+    private Transform m_Target;
+    [SerializeField]
+    private float m_Speed;
 }
