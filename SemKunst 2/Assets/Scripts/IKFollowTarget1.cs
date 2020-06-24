addd964f59767e7be725024536750c75fecfc065
Unknown <jeremy.koskamp@live.nl>
2020-06-24 13:32:30 +0200

    [/] Final Fixes

--- SemKunst 2/Assets/Scripts/IKFollowTarget1.cs
+++ SemKunst 2/Assets/Scripts/IKFollowTarget1.cs
@@ -12,11 +12,6 @@ public class IKFollowTarget1 : MonoBehaviour
     private Animator anim;
     public List<Transform> bonesToTransform;
 
-    [SerializeField]
-    private Transform m_Target;
-    [SerializeField]
-    private float m_Speed;
-
 
     private void Start()
     {
@@ -27,13 +22,9 @@ public class IKFollowTarget1 : MonoBehaviour
     {
         for (int i = 0; i < bonesToTransform.Count; i++)
         {
-            //bonesToTransform[i].LookAt(target.position);
-            //bonesToTransform[0].rotation *= Quaternion.Euler(offsetArm1);
-            //bonesToTransform[1].rotation *= Quaternion.Euler(offsetArm2);
-
-            Vector3 lTargetDir = m_Target.position - transform.position;
-            lTargetDir.y = 0.0f;
-            transform.rotation = Quaternion.RotateTowards(bonesToTransform[i].transform.rotation, Quaternion.LookRotation(lTargetDir), Time.deltaTime * m_Speed);
+            bonesToTransform[i].LookAt(target.position);
+            bonesToTransform[0].rotation *= Quaternion.Euler(offsetArm1);
+            bonesToTransform[1].rotation *= Quaternion.Euler(offsetArm2);
         }
     }   
 }
